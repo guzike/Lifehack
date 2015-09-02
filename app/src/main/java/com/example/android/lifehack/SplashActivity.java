1d52cb6463d3d9e7164b455a919f4e5790eedbbc
Eugene Guzik <guzik.evgeny@gmail.com>
2015-09-02 13:57:59 +0300

    add name activity

--- app/src/main/java/com/example/android/lifehack/SplashActivity.java
+++ app/src/main/java/com/example/android/lifehack/SplashActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
-public class Splash extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     /**
      * Duration of wait
@@ -20,15 +20,16 @@ public class Splash extends AppCompatActivity {
         super.onCreate(icicle);
         setContentView(R.layout.activity_splash);
 
+
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(Splash.this, MainActivity.class);
-                Splash.this.startActivity(mainIntent);
-                Splash.this.finish();
+                Intent mainIntent = new Intent(SplashActivity.this, NameActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
